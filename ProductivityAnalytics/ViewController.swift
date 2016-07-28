f88416b5880720deabe29e1d60b887316300aaba
Brian Nguyen <briannguyen33@gmail.com>
2016-07-28 12:55:13 -0700

    Initial Commit

---
+++ ProductivityAnalytics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProductivityAnalytics
+//
+//  Created by Brian Nguyen on 7/28/16.
+//  Copyright © 2016 BrianNguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
